1081a14f584080d84f200c6c49f9b589e31ec129
serhii_oliinyk <oleynik.sergey.ivanovi4@gmail.com>
2023-12-01 12:48:21 +0200

    lessons_03

--- js_lessons_03 (Строки, String, Методи рядків, Регулярні вирази)/youtube.js
+++ js_lessons_03 (Строки, String, Методи рядків, Регулярні вирази)/youtube.js
@@ -9,3 +9,23 @@
 // вивчіть HTML код вбудовування відео на сторінку на youtube.
 // використовуючи інтерполяцію рядків, вклейте ідентифікатор відео в HTML код вбудовування відео
 // використовуючи document.write відправте отриманий рядок на сторінку.
+
+function youtube() {
+    const userLink = prompt('Вставте посилання на Ютуб');
+    const youtbRegex = /(?:youtube\.com\/(?:[^\/\n\s]+\/\S+\/|(?:v|e(?:mbed)?)\/|\S*?[?&]v=)|youtu\.be\/)([a-zA-Z0-9_-]{11})/;
+
+    const youtbIndentificator = userLink.match(youtbRegex);
+
+    console.log(youtbIndentificator);
+
+    document.write (`
+    <div class="video">
+    <iframe  src="https://www.youtube.com/embed/${youtbIndentificator[1]}"
+        width="560" height="315"
+        frameborder="0" allowfullscreen>
+    </iframe>
+    </div>
+    `)
+
+}
+youtube();
